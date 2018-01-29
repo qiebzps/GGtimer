303c195fa68ff63647abaf49ba6a52501ce44950
zps <1033239636@qq.com>
2018-01-29 15:58:13 +0800

    v0.8

---
+++ testfps.py
@@ -0,0 +1,8 @@
+import pygame, sys, time
+fpsClodk = pygame.time.Clock()
+
+i = 0
+FPS = 1
+while i<10:
+    fpsClodk.tick(FPS)
+    i = i + 1
---
+++ testsleep.py
@@ -0,0 +1,9 @@
+import pygame, sys, time
+fpsClodk = pygame.time.Clock()
+
+i = 0
+FPS = 0.1
+while i<10:
+    #fpsClodk.tick(FPS)
+    time.sleep(1)
+    i = i + 1
